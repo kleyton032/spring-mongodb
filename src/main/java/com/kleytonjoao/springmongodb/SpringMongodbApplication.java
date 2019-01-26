ecd5fa857fcf7cba6b184ec2e3ea03ea017ec1a1
Kleyton João <kleyton.joao@gmail.com>
2019-01-26 12:10:32 -0200

    Projeto criado

---
+++ src/main/java/com/kleytonjoao/springmongodb/SpringMongodbApplication.java
@@ -0,0 +1,14 @@
+package com.kleytonjoao.springmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongodbApplication.class, args);
+	}
+
+}
+
